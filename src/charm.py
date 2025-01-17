c1964e8850b8e3f372ff3651787dbc455bf93696
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-01-17 10:40:52 -0500

    feat: add interface on core network (#7)
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- src/charm.py
+++ src/charm.py
@@ -4,13 +4,25 @@
 
 """Kubernetes Charm for iperf3."""
 
+import json
 import logging
+from typing import Any, Dict, List
 
 import ops
+from charms.kubernetes_charm_libraries.v0.multus import (
+    KubernetesMultusCharmLib,
+    NetworkAnnotation,
+    NetworkAttachmentDefinition,
+)
+from lightkube.models.meta_v1 import ObjectMeta
 
 logger = logging.getLogger(__name__)
 
 IPERF3_PORT = 5201
+CORE_GW_NAD_NAME = "core-iperf3"
+CORE_INTERFACE_NAME = "core"
+CORE_INTERFACE_BRIDGE_NAME = "core-br"
+CNI_VERSION = "0.3.1"
 
 
 class Iperf3K8SOperatorCharm(ops.CharmBase):
@@ -22,6 +34,16 @@ class Iperf3K8SOperatorCharm(ops.CharmBase):
         self._container = self.unit.get_container(self._container_name)
         self.unit.set_ports(IPERF3_PORT)
         self.framework.observe(self.on.collect_unit_status, self._on_collect_status)
+        self._kubernetes_multus = KubernetesMultusCharmLib(
+            namespace=self.model.name,
+            statefulset_name=self.model.app.name,
+            pod_name="-".join(self.model.unit.name.rsplit("/", 1)),
+            container_name=self._container_name,
+            cap_net_admin=True,
+            privileged=True,
+            network_annotations=self._generate_network_annotations(),
+            network_attachment_definitions=self._network_attachment_definitions_from_config(),
+        )
         framework.observe(self.on["iperf3"].pebble_ready, self._on_configure)
         framework.observe(self.on.config_changed, self._on_configure)
 
@@ -38,6 +60,58 @@ class Iperf3K8SOperatorCharm(ops.CharmBase):
         if plan.services != self._pebble_layer.services:
             self._container.add_layer(self._service_name, self._pebble_layer, combine=True)
             self._container.replan()
+
+    def _generate_network_annotations(self) -> List[NetworkAnnotation]:
+        """Generate a list of NetworkAnnotations to be used by Router's StatefulSet.
+
+        Returns:
+            List[NetworkAnnotation]: List of NetworkAnnotations
+        """
+        return [
+            NetworkAnnotation(
+                name=CORE_GW_NAD_NAME,
+                interface=CORE_INTERFACE_NAME,
+            ),
+        ]
+
+    def _network_attachment_definitions_from_config(self) -> list[NetworkAttachmentDefinition]:
+        """Return list of Multus NetworkAttachmentDefinitions to be created based on config.
+
+        Returns:
+            network_attachment_definitions: list[NetworkAttachmentDefinition]
+
+        """
+        core_nad_config = self._get_core_nad_config()
+
+        return [
+            NetworkAttachmentDefinition(
+                metadata=ObjectMeta(name=CORE_GW_NAD_NAME),
+                spec={"config": json.dumps(core_nad_config)},
+            ),
+        ]
+
+    def _get_core_nad_config(self) -> Dict[Any, Any]:
+        """Get core interface NAD config.
+
+        Returns:
+            config (dict): Core interface NAD config
+        """
+        config = {
+            "cniVersion": CNI_VERSION,
+            "type": "bridge",
+            "bridge": CORE_INTERFACE_BRIDGE_NAME,
+            "ipam": {
+                "type": "static",
+                "addresses": [
+                    {
+                        "address": "192.168.250.22/24",
+                    }
+                ],
+            },
+            "capabilities": {"mac": True},
+        }
+
+        return config
 
     @property
     def _pebble_layer(self) -> ops.pebble.Layer:
